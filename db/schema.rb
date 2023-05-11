186627b0af47cb6c4e34401d24e93d42451362d7
Lauren Golden <llgolden@gmail.com>
2023-05-10 22:57:54 -0600

    Add artist songs index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_10_040343) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_10_044104) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,12 +21,21 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_10_040343) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "artists", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "songs", force: :cascade do |t|
     t.string "title"
     t.integer "length"
     t.integer "play_count"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "artist_id"
+    t.index ["artist_id"], name: "index_songs_on_artist_id"
   end
 
+  add_foreign_key "songs", "artists"
 end
